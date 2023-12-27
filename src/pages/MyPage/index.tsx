d28db526571133742df98cdeaef392b1ddf4381b
99narii <122865149+99narii@users.noreply.github.com>
2023-12-27 12:29:57 +0900

    mypage

---
+++ src/pages/MyPage/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './style.css'
+
+function MyPage() {
+
+
+  return (
+    <div className='mypage'>
+        <aside className='mypageAside'>
+          <ul>
+            <li>마이페이지</li>
+          </ul>
+        </aside>
+        <div className='mypageContainer'>
+          마이페이지
+      </div>
+
+
+    </div>
+  )
+}
+
+export default MyPage
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 import IndexPage from 'pages/IndexPage';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MyPage from 'pages/MyPage';
 
 function AppRouter() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<IndexPage />} />
+				<Route path="/mypage" element={<MyPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
